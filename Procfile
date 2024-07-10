584c0625eedb23562b5fc71d9d8d736fbe4cccda
Maitreyi Shekhar Pitale <pitalemaitreyi01@gmail.com>
2024-07-09 23:44:13 -0400

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: uvicorn app.fast_api:app --host 0.0.0.0 --port $PORT
+
---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from scripts.fetch_books import fetch_book_data
+from scripts.process_books import process_book_data
+
+def main():
+    excel_file_path = os.path.join('data', 'book_names.xlsx')
+    database_path = './data/my_database.db'
+
+    # Fetch book data
+    print("Fetching book data...")
+    book_data = fetch_book_data(excel_file_path)
+
+    # Process and store book data
+    print("Processing book data...")
+    process_book_data(book_data, database_path)
+
+    print("Data processing complete.")
+
+if __name__ == "__main__":
+    main()
